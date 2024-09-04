70383da9d199ce069ffa203f015bac78c55fe56b
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2024-09-04 16:44:47 +0530

    cleanup process done i hope?

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,7 @@
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   return (
