419aa0f2c7c85d05a5382785e692972cdbee2f24
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2024-10-03 20:49:30 +0530

    we dont need all these assets?

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { Slot } from "expo-router";
-import { useFonts } from "expo-font";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+
 import * as SplashScreen from "expo-splash-screen";
 
 import "react-native-reanimated";
@@ -10,19 +10,9 @@ import "../globals.css";
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded] = useFonts({
-    SpaceMono: require("../../assets/fonts/SpaceMono-Regular.ttf"),
-  });
-
   useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
+    SplashScreen.hideAsync();
+  }, []);
 
   return (
     <GestureHandlerRootView>
