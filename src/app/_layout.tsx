31570e01b693d2469d34b10c6e863ac1117bf01d
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2024-10-03 20:27:34 +0530

    cleanup of files

---
+++ src/app/_layout.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import { Slot } from "expo-router";
+import { useFonts } from "expo-font";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import * as SplashScreen from "expo-splash-screen";
+
+import "react-native-reanimated";
+import "../globals.css";
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <GestureHandlerRootView>
+      <Slot />
+    </GestureHandlerRootView>
+  );
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default () => {
+  return (
+    <View className="flex items-center justify-center h-full w-full">
+      <Text className="text-blue-500">Hello, World!</Text>
+    </View>
+  );
+};
