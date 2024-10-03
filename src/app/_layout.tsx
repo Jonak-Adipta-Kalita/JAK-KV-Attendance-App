088cf6b649fc8fcbc0e6b5eccf60efa57fb6913d
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2024-10-03 20:39:11 +0530

    stuff looks good :D

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -11,7 +11,7 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    SpaceMono: require("../../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   useEffect(() => {
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,13 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default () => {
   return (
-    <View className="flex items-center justify-center h-full w-full">
-      <Text className="text-blue-500">Hello, World!</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View className="flex items-center justify-center h-full w-full">
+        <Text className="text-blue-500">Hello, World!</Text>
+      </View>
+    </SafeAreaView>
   );
 };
