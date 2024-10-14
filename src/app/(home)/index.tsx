2a4eb0736b8fb8d20ba4829f21f095f765fe1878
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2024-10-14 12:18:12 +0530

    fetching the data from the local metadata.json

--- src/app/(home)/index.tsx
+++ src/app/(home)/index.tsx
@@ -1,11 +1,28 @@
-import { SignedIn, useUser, useAuth } from "@clerk/clerk-expo";
 import { View } from "react-native";
+import { SignedIn, useUser, useAuth } from "@clerk/clerk-expo";
+import { ClassTeacherData } from "@/@types/typings";
+
+import classTeachersData from "@/metadata.json";
+import { useMemo } from "react";
 
 // VertualizedList for rendering large list!
 
 export default () => {
   const { user } = useUser();
   const { signOut } = useAuth();
+
+  const classTeacherData = useMemo(() => {
+    const classTeacherData: ClassTeacherData[] =
+      classTeachersData.class_teachers.map((teacher) => ({
+        ...teacher,
+        students: teacher.students.map((student) => ({
+          ...student,
+          attendance: "absent",
+        })),
+      }));
+
+    return classTeacherData.find((teacher) => teacher.id === user!.id)!;
+  }, [user!.id, classTeachersData]);
 
   return (
     <SignedIn>
